8b44309ee25656278dd492e5ad55932642f2aec8
Oesol Kim <rladhlthf123@naver.com>
2019-12-17 17:52:51 +0900

    project_contents_20191217

--- semi_code_practice2.R
+++ semi_code_practice2.R
@@ -47,14 +47,46 @@ names(sb_lib_list) # 성북구: 11개의 도서관 보유
 
 # 2-1 11개 도서관 별로 주소, 위도, 경도 내용을 담고 있는 df 추출
 lib_address = read.csv("C:/Users/student/R_semi_project/도서관정보나루_참여도서관목록.csv", header=TRUE, skip=5)
-lib_address = lib_address %>% select('참여도서관_11건','X','X.3','X.4')
-names(lib_address) = c('도서관명','주소','위도','경도')
+lib_address = lib_address %>% select('참여도서관_11건','X')
+names(lib_address) = c('도서관명','주소')
 lib_address = lib_address[-1,]
+lib_address = lib_address %>% mutate('행정동'=c('월곡2동','정릉1동','석관동','월곡2동',
+                                             '돈암2동','돈암2동','월곡2동','정릉2동',
+                                             '종암동','정릉4동','돈암2동'))
+
 View(lib_address)
 
 # 2-2 연령별, 동별 인구 통계
-human_count = read.csv('C:/Users/student/R_semi_project/서울시 주민등록인구_(연령별,동별) 통계.csv', header=TRUE)
-human_count = human_count %>% filter(자치구=='성북구') %>% filter(구분=='계')
-lib_lib_address %>% mutate('행정동'=c('월곡2동'))
-View(lib_address)
-View(human_count)
+
+# 1) 성북구 동별 총 인구
+population = read.csv('C:/Users/student/R_semi_project/서울시 주민등록인구_(연령별,동별) 통계.csv', header=TRUE)
+population = population %>% filter(자치구=='성북구') %>% filter(구분=='계')
+population = population %>% select(-기간,-자치구,-구분)
+population = population[-1,]
+View(population)
+
+# 전체 인구 대비 0~4세 인구 비율 구하기
+# population = population %>% 
+#   mutate('ratio_BabyToTotal'=
+#            round((as.numeric(as.character(population$X0.4세))/
+#                     as.numeric(as.character(population$계))*100),2))
+
+
+# 2) 성북구 동별 영아(0~4세) 인구
+# population_baby = population %>% select('행정동','X0.4세','ratio_BabyToTotal')
+# 
+# View(population_baby)
+
+# 3) 성북구 도서관별 행정동, 그리고 행정동 별로 인구 수치, 비율
+sb_adr_plus_pop = inner_join(lib_address,population,by="행정동")
+View(sb_adr_plus_pop)
+
+
+# 3 2018 성북구 도서관 데이터
+setwd("C:/Rstudy/R_semi_project_data")
+gu = 'seongbook'
+file_name = paste('2018', gu, 'library cumsum data.rds')
+sb_cumsum_list = readRDS(file_name)
+nrow(sb_cumsum_list$달빛마루도서관[[1]])
+
+View(sb_cumsum_list)
